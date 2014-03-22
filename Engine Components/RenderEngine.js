7bebedf8399956b0baa3f81bd45e5b812b889f1b
Karthik NS <karthik.nallapeta@gmail.com>
2014-03-22 15:29:32 -0700

    RenderEngine: Cleaned architecture of AnimRederer
    Cleaned architecture of AnimRederer
    Test file displays usage of frame rate param for AnimRenderer

--- Engine Components/RenderEngine.js
+++ Engine Components/RenderEngine.js
@@ -97,51 +97,57 @@ ImageRenderer.prototype.render = function(context)
 
 function AnimRenderer(imageNames, frameRate)
 {
-	this.x = 0;
-	this.y = 0;
 	this.images = new Array(imageNames.length);
-	this.frameRate = frameRate;
-
-	this.isAnimStarted = false;
-	
-	this.lastStartTime = 0; // milliseconds
-	this.frameDuration =  Math.floor(1000 / frameRate); // milliseconds
-	this.totalAnimTime = imageNames.length * this.frameDuration; // milliseconds
 
 	for(var index = 0; index < imageNames.length; ++index)
 	{
 		this.images[index] = imageRepository.getImage(imageNames[index]);
 	}
 
-	this.startAnimation = function()
+	this.frameRate = frameRate;
+	this.frameDuration =  Math.floor(1000 / frameRate); // milliseconds
+	this.totalAnimTime = imageNames.length * this.frameDuration; // milliseconds
+}
+
+AnimRenderer.prototype.x = 0;
+AnimRenderer.prototype.y = 0;
+AnimRenderer.prototype.images = null;
+
+AnimRenderer.prototype.isAnimStarted = false;
+AnimRenderer.prototype.frameRate = 0;		// FPS
+AnimRenderer.prototype.lastStartTime = 0; 	// milliseconds
+AnimRenderer.prototype.frameDuration = 0;	// milliseconds
+AnimRenderer.prototype.totalAnimTime = 0;	// milliseconds
+
+AnimRenderer.prototype.startAnimation = function()
+{
+	var date = new Date();
+	this.lastStartTime = date.getTime();
+	this.isAnimStarted = true;
+}
+
+AnimRenderer.prototype.stopAnimation = function()
+{
+	this.isAnimStarted = false;
+}
+
+AnimRenderer.prototype.render = function(context)
+{
+	var image = this.images[0];
+
+	if(this.isAnimStarted == true)
 	{
 		var date = new Date();
-		this.lastStartTime = date.getTime();
-		this.isAnimStarted = true;
+		var currentTime = date.getTime();
+
+		var animationsElapsed = Math.floor((currentTime - this.lastStartTime) / this.totalAnimTime);
+		this.lastStartTime = this.lastStartTime +  animationsElapsed * this.totalAnimTime;
+		var currentFrame = Math.floor( (currentTime - this.lastStartTime) / this.frameDuration );
+		
+		image = this.images[currentFrame];
 	}
 
-	this.stopAnimation = function()
-	{
-		this.isAnimStarted = false;
-	}
-
-	this.render = function(context)
-	{
-		var image = this.images[0];
-	
-		if(this.isAnimStarted == true)
-		{
-			var date = new Date();
-			var currentTime = date.getTime();
-			
-			this.lastStartTime = this.lastStartTime + Math.floor((currentTime - this.lastStartTime) / this.totalAnimTime) * this.totalAnimTime;
-			var currentFrame = Math.floor( (currentTime - this.lastStartTime) / this.frameDuration );
-			
-			image = this.images[currentFrame];
-		}
-
-		context.drawImage(image, this.x, this.y);
-	}
+	context.drawImage(image, this.x, this.y);
 }
 
 function AnimManager()
